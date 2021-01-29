0257ceeb30591be8eb3d3dbc74d9cd0f820e3d7e
amygdala <ansb1987@gmail.com>
2021-01-29 20:13:19 +0800

    Completed Desktop/Mobile header styles

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,9 @@
 import "../styles/styles.css";
 import "lazysizes";
+import MobileMenu from "./modules/MobileMenu.js";
 import "./ls.respimg.js";
+
+let mobileMenu = new MobileMenu();
 
 if (module.hot) {
   module.hot.accept();
---
+++ app/assets/scripts/modules/MobileMenu.js
@@ -0,0 +1,18 @@
+class MobileMenu {
+  constructor() {
+    this.menuIcon = document.querySelector(".site-header__hamburger");
+    this.menuContent = document.querySelector(".site-header__menu-content");
+    this.siteHeader = document.querySelector(".site-header");
+    this.events();
+  }
+  events() {
+    this.menuIcon.addEventListener("click", () => this.toggleMenu());
+  }
+  toggleMenu() {
+    this.menuContent.classList.toggle("site-header__menu-content--is-visible");
+    this.siteHeader.classList.toggle("site-header--is-expanded");
+    this.menuIcon.classList.toggle("site-header__hamburger--close-x");
+  }
+}
+
+export default MobileMenu;
