7f6fbf867325748fabdec844d85c9fe0a144269b
amygdala <ansb1987@gmail.com>
2021-01-31 17:34:00 +0800

    completed modal-overlay

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -5,11 +5,27 @@ import MobileMenu from "./modules/MobileMenu.js";
 import "./ls.respimg.js";
 import StickyHeader from "./modules/StickyHeader.js";
 
-let stickyHeader = new StickyHeader();
+new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".story-item"), 50);
+new MobileMenu();
+let modal;
 
-let mobileMenu = new MobileMenu();
+document.querySelectorAll(".open-modal").forEach((el) => {
+  el.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (typeof modal == "undefined") {
+      import(/* webpackChunkName: "modal" */ "./modules/Modal.js")
+        .then((x) => {
+          modal = new x.default();
+          setTimeout(() => modal.openTheModal(), 20);
+        })
+        .catch(() => console.log("error loading event."));
+    } else {
+      modal.openTheModal();
+    }
+  });
+});
 
 if (module.hot) {
   module.hot.accept();
